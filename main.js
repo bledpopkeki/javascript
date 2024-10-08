92bd422210178a18fd292eb0eb71cadae0863682
bledpopkeki <bledh4352@gmail.com>
2024-10-08 16:04:04 +0200

    wef

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 var a = 10
+var b = 40
+var c = 30
+var d = 20
+console.log(a==b && c<b)
+console.log(!(a==b && c<b))
+console.log(a==b || c<b)
+
+
 console.log(a)
 a++;
 console.log(!(a))
+('my school name is ' + ${school});
