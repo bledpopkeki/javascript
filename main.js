f85a39b43faea452186b546a949176f80cc36ad6
bledpopkeki <bledh4352@gmail.com>
2024-10-17 07:17:48 -0700

    asd

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
-
-var button1=document.getElementById("b1");
-var button2=document.getElementById("b2");
-var button3=document.getElementById("b3");
-var ds_text=document.getElementById("txt1");
-
-button1.onclick=function(){
-    ds_text.style.color="blue";
-    ds_text.style.textAlign = "center";
-    ds_text.style.fontSize = "50px";
-    js_text.setAtrribute('class','test')
-
-}
-button2.onmouseover=function(){
-    alert("you hovered over me");
-}
-button3.onmouseleave=function(){
-    ds_text.style.cssText = "color:red; text-align: right; font-size: 100px; background-color: lightblue;"
-}
-
-
-
-
-
-
-
+var brands=["samsung", "apple", "nike", "adidas", "lacoste", "dell", "lenovo", "LG", "asus", "shkolla digjitale", "nokia"];
+console.log(brands[4]);
+brands.push('1000000')
+console.log(brands)
+brands.pop();
+brands.shift("blackberry");
+console.log(brands);
+brands.unshift();
+console.log(brands);
+brands.splice(1,0, "1")
 
 
 
