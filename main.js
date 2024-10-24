55f2ea819f6a913d38ffddbd691477511eb9ef31
bledpopkeki <bledh4352@gmail.com>
2024-10-24 07:18:49 -0700

    ad

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-var brands=["samsung", "apple", "nike", "adidas", "lacoste", "dell", "lenovo", "LG", "asus", "shkolla digjitale", "nokia"];
-console.log(brands[4]);
-brands.push('1000000')
-console.log(brands)
-brands.pop();
-brands.shift("blackberry");
-console.log(brands);
-brands.unshift();
-console.log(brands);
-brands.splice(1,0, "1")
+var input=document.getElementById('input');
+var button=document.getElementById('btn');
+var text=document.getElementById('text');
+var button1=document.getElementById('plus');
+var input=document.getElementById('n1');
+var input=document.getElementById('n2');
+var h2=document.getElementById('h2');
+var age=document.getElementById('age')
+
+button.onclick=function(){
+    text.innerHTML=input.value;
+}
+    button1.onclick=function(){
+
+        h2.innerHTML=parseInt(input.value)+ parseInt(input2.value);
+    }
+if( age>= 18 ){
 
 
-
-
-
-
-
-
-
-
-
-
-
+}
 
