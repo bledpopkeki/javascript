31b4ad9e996f39ee9a56b10ce581e68b3a2d89a0
bledpopkeki <bledh4352@gmail.com>
2024-12-05 16:08:42 +0100

    asdsdgdfgdfg

--- module36/main.js
+++ module36/main.js
@@ -1,11 +1,17 @@
-$("li").click(function(){
-    alert("li has been clicked")
-})
-$("#h1").click(function(){
-    $("#h1").text("anik dhe bled")
-    $("#h1").append(" edhe nil")
-})
 
-$('#id1').click(function(){
-$(this).addClass("clicked");
-}).find("span").attr("title", "this is a span");
+$("#showbutton").click(function(){
+    $("#hidden").show(5000);
+
+})
+$("#hidebutton").click(function(){
+    $("#hidden").hide();
+})
+$(".animate").click(function(){
+    $(this).animate({
+        "left" : "+=50px",
+        "width" : "400px",
+        "height" : "400px",
+        "fontSize" : "50px",
+        "opacity" : "-=0.1"
+});
+})
