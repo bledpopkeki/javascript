e910c9202a16bddf17e6900a624a3a9050b1b9f4
bledpopkeki <bledh4352@gmail.com>
2024-12-10 15:53:08 +0100

    dsff

--- module36/main.js
+++ module36/main.js
@@ -1,17 +1,4 @@
-
-$("#showbutton").click(function(){
-    $("#hidden").show(5000);
-
-})
-$("#hidebutton").click(function(){
-    $("#hidden").hide();
-})
-$(".animate").click(function(){
-    $(this).animate({
-        "left" : "+=50px",
-        "width" : "400px",
-        "height" : "400px",
-        "fontSize" : "50px",
-        "opacity" : "-=0.1"
-});
-})
+$("#sort").sortable();
+$("#accordion").accordion({
+    collapsible:true});
+$("#tabs").tabs();
